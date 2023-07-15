582f96d3ddbc313f431cfabd6ea4784c8b008b8b
Surendra Reddy <dasarisurendrareddy123@gmail.com>
2023-07-15 01:08:39 +0100

    commit1

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,88 +1,67 @@
 provider "azurerm" {
-    features {}
+  features {}
 }
 
-# Create a resource group
 resource "azurerm_resource_group" "example" {
   name     = "example-resource-group"
   location = "West Europe"
 }
 
-# Create an App Service Plan
-resource "azurerm_app_service_plan" "example" {
-  name                = "example-appserviceplan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  kind                = "Linux"
-
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
-}
-
-# Create an Azure Storage Account
 resource "azurerm_storage_account" "example" {
   name                     = "examplestorage"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
-  tags = {
-    environment = "dev"
-  }
 }
 
-# Create an Azure Key Vault
 resource "azurerm_key_vault" "example" {
-  name                = "examplekeyvault"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  name                        = "examplekeyvault"
+  resource_group_name         = azurerm_resource_group.example.name
+  location                    = azurerm_resource_group.example.location
   enabled_for_disk_encryption = true
-  tenant_id           = data.azurerm_client_config.current.tenant_id
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+}
 
-  sku_name = "standard"
-  
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
+data "azurerm_client_config" "current" {}
 
-    secret_permissions = [
-      "get",
-      "list",
-    ]
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-app-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
   }
 }
 
-# Create a Web App
-resource "azurerm_app_service" "example" {
-  name                = "example-webapp"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
+resource "azurerm_linux_web_app" "example" {
+  name                      = "example-web-app"
+  location                  = azurerm_resource_group.example.location
+  resource_group_name       = azurerm_resource_group.example.name
+  app_service_plan_id       = azurerm_app_service_plan.example.id
+  app_settings              = { "SOME_KEY" = "SOME_VALUE" }
+  client_affinity_enabled   = false
+  always_on                 = true
+  connection_string         = azurerm_key_vault_secret.example_connection_string.id
+  https_only                = true
+  min_tls_version           = "1.2"
+  ftps_state                = "Disabled"
+  use_32_bit_worker_process = false
 
   site_config {
-    always_on = true
-
-    app_settings = [
-      {
-        name  = "STORAGE_ACCOUNT_NAME"
-        value = azurerm_storage_account.example.name
-      },
-      {
-        name  = "KEY_VAULT_NAME"
-        value = azurerm_key_vault.example.name
-      },
-    ]
-  }
-
-  identity {
-    type = "SystemAssigned"
+    always_on               = true
+    linux_fx_version        = "DOCKER|nginx"
+    use_32_bit_worker_process = false
   }
 }
 
-# Output the Web App URL
-output "webapp_url" {
-  value = azurerm_app_service.example.default_site_hostname
+resource "azurerm_key_vault_secret" "example_connection_string" {
+  name         = "ExampleConnectionString"
+  value        = azurerm_storage_account.example.primary_connection_string
+  key_vault_id = azurerm_key_vault.example.id
 }
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,13 +1,19 @@
+variable "client_id" {
+  description = "The Azure AD Client ID"
+  type        = string
+}
 
-variable "ARM_TENANT_ID"{
+variable "client_secret" {
+  description = "The Azure AD Client Secret"
+  type        = string
+}
 
+variable "subscription_id" {
+  description = "The Azure Subscription ID"
+  type        = string
 }
-variable "ARM_SUBSCRIPTION_ID"{
-  
-}
-variable "ARM_CLIENT_ID"{
-  
-}
-variable "ARM_CLIENT_SECRET"{
-  
+
+variable "tenant_id" {
+  description = "The Azure AD Tenant ID"
+  type        = string
 }
